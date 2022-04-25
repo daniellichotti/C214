9131997f807dc003f7b3c02a70365bff994954bb
unknown <danielclichotti@gmail.com>
2022-04-25 20:42:17 -0300

    Removendo

--- Aula 8 e 9 Exercícios/server.js
+++
@@ -1,6 +0,0 @@
-require('./src/infrastructure/database');
-const app = require('./src/infrastructure/rest_server');
-
-app.listen(5000, () => {
-    console.log('REST server running on port 5000...');
-});
